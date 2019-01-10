723ebd5d2c8fd1a62733276b35f793804efde2b5
emkozuch <emkozuch@gmail.com>
2019-01-11 00:51:12 +0100

    second

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
-document.querySelector('.open-modal').addEventListener('click', function(){
+document.querySelector('.open-modal').addEventListener('click', toggleClass);
+document.querySelector('.close-modal').addEventListener('click', toggleClass);
+ 
+function toggleClass(){
     document.querySelector('.modal-background').classList.toggle('visible');
-})
-document.querySelector('.close-modal').addEventListener('click', function(){
-    document.querySelector('.modal-background').classList.toggle('visible');
-})
-document.querySelector('.one').addEventListener('click', function(){
-    console.log(this.dataset.color);
-})
-document.querySelector('.two').addEventListener('click', function(){
-    console.log(this.dataset.color);
-})
-document.querySelector('.three').addEventListener('click', function(){
-    console.log(this.dataset.color);
-})
-document.querySelector('.four').addEventListener('click', function(){
-    console.log(this.dataset.color);
-});
+}
+
+
 
 const rectangleArea = document.querySelector('.rectangles-container');
-
+const smallBtn = document.querySelector('.size-small');
+const mediumBtn = document.getElementById('radio-medium');
+const largeBtn = document.getElementById('radio-large');
+const colorOne = document.querySelector('.one');
+const colorTwo = document.querySelector('.two');
+const colorThree = document.querySelector('.three');
+const colorFour = document.querySelector('.four');
+const colorFive = document.querySelector('.five');
+const colorSix = document.querySelector('.six');
+const startBtn = document.querySelector('.position-start');
+const endBtn = document.querySelector('.position-end');
+const radioBtn = document.getElementById('radio-small');
 document.querySelector('.lol').addEventListener('click', createRectangle);
 
 function createRectangle(){
@@ -26,7 +27,26 @@ function createRectangle(){
     rectangleDOM.classList = 'large-rec';
     rectangleArea.appendChild(rectangleDOM);
 }
+let someArray = [];
+smallBtn.addEventListener('click', function(){
+   if (radioBtn){
+       someArray = [radioBtn.dataset.size];
+   }
+})
+mediumBtn.addEventListener('click', function(){
+    if (mediumBtn){
+        someArray = [mediumBtn.dataset.size]
+    }
+})
+largeBtn.addEventListener('click', function(){
+    if (largeBtn){
+        someArray = [largeBtn.dataset.size]
+    }
+})
 
 
 
-    
+
+
+
+
