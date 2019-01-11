5d6945a32b926c79faa6ebf38480a126870b639f
emkozuch <emkozuch@gmail.com>
2019-01-11 02:34:16 +0100

    made some rectangles append

--- script.js
+++ script.js
@@ -1,47 +1,71 @@
-document.querySelector('.open-modal').addEventListener('click', toggleClass);
-document.querySelector('.close-modal').addEventListener('click', toggleClass);
+document.querySelector('.open-modal').addEventListener('click', toggleModalClass);
+document.querySelector('.close-modal').addEventListener('click', toggleModalClass);
  
-function toggleClass(){
+function toggleModalClass(){
     document.querySelector('.modal-background').classList.toggle('visible');
 }
 
 
 
 const rectangleArea = document.querySelector('.rectangles-container');
-const smallBtn = document.querySelector('.size-small');
+const smallBtn = document.getElementById('radio-small');
 const mediumBtn = document.getElementById('radio-medium');
 const largeBtn = document.getElementById('radio-large');
-const colorOne = document.querySelector('.one');
-const colorTwo = document.querySelector('.two');
-const colorThree = document.querySelector('.three');
-const colorFour = document.querySelector('.four');
-const colorFive = document.querySelector('.five');
-const colorSix = document.querySelector('.six');
-const startBtn = document.querySelector('.position-start');
-const endBtn = document.querySelector('.position-end');
-const radioBtn = document.getElementById('radio-small');
-document.querySelector('.lol').addEventListener('click', createRectangle);
+const colorOne = document.getElementById('radio-color-one');
+const colorTwo = document.getElementById('radio-color-two');
+const colorThree = document.getElementById('radio-color-three');
+const colorFour = document.getElementById('radio-color-four');
+const colorFive = document.getElementById('radio-color-five');
+const colorSix = document.getElementById('radio-color-six');
+const startBtn = document.getElementById('.position-start');
+const endBtn = document.getElementById('.position-end');
 
-function createRectangle(){
-    const rectangleDOM = document.createElement('div');
-    rectangleDOM.classList = 'large-rec';
-    rectangleArea.appendChild(rectangleDOM);
+
+let properties = {
+    size: '',
+    color: '',
+    position: ''
 }
-let someArray = [];
+   function Rectangle(size,color,position){
+       this.size = size
+       this.color = color
+       this.position = position
+   } 
+
 smallBtn.addEventListener('click', function(){
-   if (radioBtn){
-       someArray = [radioBtn.dataset.size];
+   if (smallBtn){
+       properties.size = smallBtn.dataset.size;
    }
 })
 mediumBtn.addEventListener('click', function(){
     if (mediumBtn){
-        someArray = [mediumBtn.dataset.size]
+        properties.size = mediumBtn.dataset.size
     }
 })
 largeBtn.addEventListener('click', function(){
     if (largeBtn){
-        someArray = [largeBtn.dataset.size]
+        properties.size = largeBtn.dataset.size
     }
+})
+let selectedColor = ''
+colorOne.addEventListener('click', function(){
+    if (colorOne){
+        properties.color = colorOne.dataset.color;
+        selectedColor = properties.color;
+    }
+})
+colorTwo.addEventListener('click', function(){
+    if (colorTwo){
+        properties.color = colorTwo.dataset.color;
+        selectedColor = properties.color;
+    }
+})
+
+document.querySelector('.confirm').addEventListener('click', function(){
+   const rec = document.createElement('div')
+   rec.classList = properties.size + '-rec'
+   rec.style.backgroundColor = selectedColor ;
+   rectangleArea.appendChild(rec)
 })
 
 
