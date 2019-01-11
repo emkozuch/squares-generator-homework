3a5c31b860e8031f750b1ff8b7f6d59001e248eb
emkozuch <emkozuch@gmail.com>
2019-01-11 15:58:58 +0100

    almost final version

--- script.js
+++ script.js
@@ -1,76 +1,52 @@
-document.querySelector('.open-modal').addEventListener('click', toggleModalClass);
-document.querySelector('.close-modal').addEventListener('click', toggleModalClass);
- 
-function toggleModalClass(){
-    document.querySelector('.modal-background').classList.toggle('visible');
+document.querySelector('.open-modal').addEventListener('click', toggleModalClass)
+document.querySelector('.close-modal').addEventListener('click', toggleModalClass)
+
+function toggleModalClass() {
+    document.querySelector('.modal-background').classList.toggle('visible')
 }
 
 
-
-const rectangleArea = document.querySelector('.rectangles-container');
-const smallBtn = document.getElementById('radio-small');
-const mediumBtn = document.getElementById('radio-medium');
-const largeBtn = document.getElementById('radio-large');
-const colorOne = document.getElementById('radio-color-one');
-const colorTwo = document.getElementById('radio-color-two');
-const colorThree = document.getElementById('radio-color-three');
-const colorFour = document.getElementById('radio-color-four');
-const colorFive = document.getElementById('radio-color-five');
-const colorSix = document.getElementById('radio-color-six');
-const startBtn = document.getElementById('.position-start');
-const endBtn = document.getElementById('.position-end');
-
+const recQueue = document.querySelector('.rec-queue');
+const rectangleArea = document.querySelector('.rec-render')
 
 let properties = {
     size: '',
     color: '',
     position: ''
 }
-   function Rectangle(size,color,position){
-       this.size = size
-       this.color = color
-       this.position = position
-   } 
-
-smallBtn.addEventListener('click', function(){
-   if (smallBtn){
-       properties.size = smallBtn.dataset.size;
-   }
-})
-mediumBtn.addEventListener('click', function(){
-    if (mediumBtn){
-        properties.size = mediumBtn.dataset.size
+document.getElementById('parent-size').addEventListener('click', function (e) {
+    if (e.target) {
+        properties.size = e.target.dataset.size
     }
 })
-largeBtn.addEventListener('click', function(){
-    if (largeBtn){
-        properties.size = largeBtn.dataset.size
+document.getElementById('parent-color').addEventListener('click', function (e) {
+    if (e.target) {
+        properties.color = e.target.dataset.color
     }
 })
-let selectedColor = ''
-colorOne.addEventListener('click', function(){
-    if (colorOne){
-        properties.color = colorOne.dataset.color;
-        selectedColor = properties.color;
-    }
-})
-colorTwo.addEventListener('click', function(){
-    if (colorTwo){
-        properties.color = colorTwo.dataset.color;
-        selectedColor = properties.color;
+document.getElementById('parent-position').addEventListener('click', function (e) {
+    if (e.target) {
+        properties.position = e.target.dataset.position
     }
 })
 
-document.querySelector('.confirm').addEventListener('click', function(){
-   const rec = document.createElement('div')
-   rec.classList = properties.size + '-rec'
-   rec.style.backgroundColor = selectedColor ;
-   rectangleArea.appendChild(rec)
+document.querySelector('.confirm').addEventListener('click', function () {
+    const rec = document.createElement('div')
+    const recVisualisation = document.createElement('div')
+
+    rec.classList = properties.size + '-rec' + ' ' + 'recDOM'
+    rec.style.backgroundColor = properties.color
+
+    recVisualisation.classList = properties.size + '-rec' + ' ' + 'recDOM-visualised'
+    recVisualisation.style.backgroundColor = properties.color
+  
+    if (properties.position === 'start') {
+        rectangleArea.prepend(rec)
+        recQueue.prepend(recVisualisation)
+    } else if (properties.position === 'end') {
+        rectangleArea.appendChild(rec)
+        recQueue.appendChild(recVisualisation)
+    }
 })
-
-
-
-
-
 
 
